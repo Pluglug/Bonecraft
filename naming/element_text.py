d95e680e7882033384b071b91e38a5fd44f6db56
Pluglug <upward571103@gmail.com>
2024-01-20 17:53:01 +0900

    Add note on how to detect UnknownElement

--- naming/element_text.py
+++ naming/element_text.py
@@ -16,6 +16,8 @@ except:
 #     def build_pattern(self):
 #         # TODO: プリセットに無い語を検出可能にする
 #         return None
+#     # 検出時に、セパレーターで分割しながらorderを割り振る。
+#     # Elementのorderは、検出時に一時的に上書きされる。
 
 
 class TextElement(NamingElement):
